f7a6a763039394fbd3435739e998779c5d915716
Ayush Bulbule <ayushbulbule24@gmail.com>
2023-02-13 19:48:00 +0530

    Added Login

--- src/frontend/components/LoginPage.js
+++ src/frontend/components/LoginPage.js
@@ -10,18 +10,18 @@ const LoginPage = () => {
         <h4 className="text-2xl text-center font-sans font-semibold text-white">Login</h4>
         <div className="grid md:grid-cols-2 md:grid-rows-1 sm:grid-rows-2 gap-2 items-center">
 
-          <div className="py-12 ">
+          <div className="md:py-12 py-4 ">
             <img src={login_img} alt="Login" />
           </div>
           <div className="form">
             <form action="/">
-              <div className="mb-4">
-                <label className="block text-white text-sm font-bold mb-2" for="username">
+              <div className="mb-3">
+                <label className="mt-4 block text-white text-sm font-bold mb-2" for="username">
                   Username
                 </label>
                 <input className="w-full py-1.5 px-4 mt-2 bg-slate-500/50 rounded-md outline-none font-semibold text-white focus:border-slate-800 focus:outline-cyan-400" id="username" type="text" placeholder="Username" />
               </div>
-              <button className="py-1.5 px-4 mt-5 w-full  bg-cyan-400 text-black rounded-md font-medium ml-auto items-end hover:bg-cyan-600">
+              <button className="py-1.5 px-4 mt-3 w-full  bg-cyan-400 text-black rounded-md font-medium ml-auto items-end hover:bg-cyan-600">
                 Sign In
               </button>
 
@@ -32,13 +32,13 @@ const LoginPage = () => {
               <span class="px-3 font-medium    left-1/2 text-white">or</span>
               <span class="h-0.5  my-8 bg-gray-400 w-full border-0 "></span>
             </div>
-            <button className="py-1.5 px-4  flex items-center justify-center mt-5 w-full  bg-white shadow-sm text-black rounded-md font-medium ml-auto items-end hover:bg-cyan-600">
+            <button className="py-1.5 px-4  flex items-center justify-center  w-full  bg-white shadow-sm text-black rounded-md font-medium hover:bg-cyan-600">
               <FcGoogle class="text-2xl" />
               <span className="text-sm ml-2">
                 SinIn with Google
               </span>
             </button>
-            <button className="py-1.5 px-4  flex items-center justify-center mt-5 w-full  bg-white shadow-sm text-black rounded-md font-medium ml-auto items-end hover:bg-cyan-600">
+            <button className="py-1.5 px-4  flex items-center justify-center mt-3 w-full  bg-white shadow-sm text-black rounded-md font-medium hover:bg-cyan-600">
               <FaDiscord class="text-2xl text-blue-500" />
               <span className="text-sm ml-2">
                 SignIn with Discord
