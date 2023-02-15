d9672798c3f242eafcc00aedcec8a2b425b2d87e
Ayush Bulbule <ayushbulbule24@gmail.com>
2023-02-15 12:51:46 +0530

    Home Page

--- src/frontend/components/Home.js
+++ src/frontend/components/Home.js
@@ -1,21 +1,30 @@
 import React from 'react'
 import { Navbar } from './Navbar';
-import nft from "./Public/nft.png";
+import nft from "./Public/h.png";
+import TimeLine from './TimeLine';
 
 export const Home = () => {
     return (
         <>
-        <div className='bg-rbgradient'>
-        <Navbar />
-
-
-            <div className='flex justify-center items-center mx-32 space-x-28 h-screen overflow-y-hidden'>
-                <p className='text-justify text-white text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem dolor, quae molestiae possimus sit repellendus molestias reprehenderit veniam exercitationem obcaecati numquam quibusdam? Quisquam molestiae distinctio totam possimus! Distinctio, sed reiciendis!</p>
-                <img src={nft} alt="" className='h-[85%]' />
+            <Navbar />
+            <div className='bg-rbgradient min-h-screen py-12'>
+                <div className="flex flex-col md:flex-row md:px-16 container text-gray-200 mx-auto items-center justify-center ">
+                    <div className="w-full px-4 md:w-3/6 flex order-2 md:order-1 flex-col items-start justify-start">
+                        <p className="text-xl font-semibold capitalize ml-1 py-4 text-rose-600">ENS MARKETPLACE</p>
+                        <h1 className="text-4xl md:text-6xl font-extrabold">Buy, sell, and own domains</h1>
+                        <p className="text-md text-gray-400 mt-4">Decentralizing the Domain Name System: The Future of Secure and User-Friendly Blockchain Naming System for the Ethereum Ecosystem</p>
+                        <div className="flex flex-row items-center justify-start mt-8">
+                            <button className="bg-red-600 rounded-full text-white px-8 hover:bg-red-700 py-2 font-semibold">Buy Now</button>
+                            </div>
+                    </div>
+                    <div className="w-full md:w-3/6 mt-24 px-16 md:px-8 md:mt-12 order-1 md:order-1 flex flex-col items-start justify-start">
+                        <img src={nft} />
+                    </div>
+                </div>
+               
             </div>
-        </div>
+                <TimeLine/>
         </>
     )
-}
 
-// bg-gradient-to-t from-purple-900 to-violet-900
+}
---
+++ src/frontend/components/TimeLine.js
@@ -0,0 +1,96 @@
+import React from 'react'
+
+const TimeLine = () => {
+    return (
+
+        <div className="timeline flex  bg-slate-900 text-light">
+        <div className="flex flex-col md:flex-row container mt-5 px-8 md:px-16 mx-auto">
+
+            <div className="w-full md:w-1/3 md:px-8 py-8"><h3 className='text-xl font-bold text-gray-200 '>5 Easy Steps to get your ENS</h3></div>
+            <div className="w-full px-8 md:px-0 md:w-2/3">
+            <ol className="border-l-2 border-blue-600">
+                <li>
+                    <div className="flex flex-start items-center">
+                        <div className="bg-blue-600 w-8 h-8  flex items-center justify-center rounded-full -ml-4 mr-3 -mt-2">
+                            <div className='w-6 h-6  bg-slate-900 rounded-full flex items-center justify-center font-semibold text-gray-100'>
+                                <div className="text-xs text-gray-100">1</div>
+                            </div>
+                        </div>
+                        <h4 className="text-gray-200 font-semibold text-xl -mt-2">Title of section 1</h4>
+                    </div>
+                    <div className="ml-6 mb-6 pb-6">
+                        <a href="#!" className="text-blue-600 hover:text-blue-700 focus:text-blue-800 duration-300 transition ease-in-out text-sm">4 February, 2022</a>
+                        <p className="text-gray-300 mt-2 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <button type="button" className="inline-block px-4 py-1.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Read more</button>
+                    </div>
+                </li>
+                <li>
+                    <div className="flex flex-start items-center">
+                        <div className="bg-blue-600 w-8 h-8  flex items-center justify-center rounded-full -ml-4 mr-3 -mt-2">
+                            <div className='w-6 h-6  bg-slate-900 rounded-full flex items-center justify-center font-semibold text-gray-100'>
+                                <div className="text-xs text-gray-100">2</div>
+                            </div>
+                        </div>
+                        <h4 className="text-gray-200 font-semibold text-xl -mt-2">Title of section 2</h4>
+                    </div>
+                    <div className="ml-6 mb-6 pb-6">
+                        <a href="#!" className="text-blue-600 hover:text-blue-700 focus:text-blue-800 duration-300 transition ease-in-out text-sm">4 February, 2022</a>
+                        <p className="text-gray-300 mt-2 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <button type="button" className="inline-block px-4 py-1.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Read more</button>
+                    </div>
+                </li>
+                <li>
+                    <div className="flex flex-start items-center">
+                        <div className="bg-blue-600 w-8 h-8  flex items-center justify-center rounded-full -ml-4 mr-3 -mt-2">
+                            <div className='w-6 h-6  bg-slate-900 rounded-full flex items-center justify-center font-semibold text-gray-100'>
+                                <div className="text-xs text-gray-100">3</div>
+                            </div>
+                        </div>
+                        <h4 className="text-gray-200 font-semibold text-xl -mt-2">Title of section 3</h4>
+                    </div>
+                    <div className="ml-6 mb-6 pb-6">
+                        <a href="#!" className="text-blue-600 hover:text-blue-700 focus:text-blue-800 duration-300 transition ease-in-out text-sm">4 February, 2022</a>
+                        <p className="text-gray-300 mt-2 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <button type="button" className="inline-block px-4 py-1.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Read more</button>
+                    </div>
+                </li>
+                <li>
+                    <div className="flex flex-start items-center">
+                        <div className="bg-blue-600 w-8 h-8  flex items-center justify-center rounded-full -ml-4 mr-3 -mt-2">
+                            <div className='w-6 h-6  bg-slate-900 rounded-full flex items-center justify-center font-semibold text-gray-100'>
+                                <div className="text-xs text-gray-100">4</div>
+                            </div>
+                        </div>
+                        <h4 className="text-gray-200 font-semibold text-xl -mt-2">Title of section 4</h4>
+                    </div>
+                    <div className="ml-6 mb-6 pb-6">
+                        <a href="#!" className="text-blue-600 hover:text-blue-700 focus:text-blue-800 duration-300 transition ease-in-out text-sm">4 February, 2022</a>
+                        <p className="text-gray-300 mt-2 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <button type="button" className="inline-block px-4 py-1.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Read more</button>
+                    </div>
+                </li>
+                <li>
+                    <div className="flex flex-start items-center">
+                        <div className="bg-blue-600 w-8 h-8  flex items-center justify-center rounded-full -ml-4 mr-3 -mt-2">
+                            <div className='w-6 h-6  bg-slate-900 rounded-full flex items-center justify-center font-semibold text-gray-100'>
+                                <div className="text-xs text-gray-100">5</div>
+                            </div>
+                        </div>
+                        <h4 className="text-gray-200 font-semibold text-xl -mt-2">Title of section 5</h4>
+                    </div>
+                    <div className="ml-6 mb-6 pb-6">
+                        <a href="#!" className="text-blue-600 hover:text-blue-700 focus:text-blue-800 duration-300 transition ease-in-out text-sm">4 February, 2022</a>
+                        <p className="text-gray-300 mt-2 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <button type="button" className="inline-block px-4 py-1.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Read more</button>
+                    </div>
+                </li>
+                
+            </ol></div>
+        </div>
+        </div>
+
+
+    )
+}
+
+export default TimeLine
