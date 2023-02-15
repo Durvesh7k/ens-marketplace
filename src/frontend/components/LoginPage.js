d4993afebade91a4f2e0a68605cf8fac39d1745c
Durvesh7k <dschopade7k@gmail.com>
2023-02-15 15:09:49 +0530

    The blockchain integration is done

--- src/frontend/components/LoginPage.js
+++ src/frontend/components/LoginPage.js
@@ -3,7 +3,24 @@ import { Link } from 'react-router-dom'
 import login_img from "./Public/login-img.png"
 import { FcGoogle } from "react-icons/fc"
 import { FaDiscord } from "react-icons/fa"
-const LoginPage = () => {
+import { useState } from 'react'
+
+
+
+const LoginPage = ({loginWithLink,login}) => {
+
+  const [email, setEmail] = useState("");
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handelSubmit = (e) => {
+    e.preventdefault();
+    loginWithLink(email);
+  }
+
+
   return (
     <div className="bg-login h-screen w-100 p-8 flex items-center justify-center">
       <div className="md:w-3/6 sm:w-full  rounded-md shadow-sm shadow-slate-800 bg-slate-600/30 p-8">
@@ -14,14 +31,20 @@ const LoginPage = () => {
             <img src={login_img} alt="Login" />
           </div>
           <div className="form">
-            <form action="/">
+            <form action="/" onSubmit={handelSubmit}>
               <div className="mb-3">
-                <label className="mt-4 block text-white text-sm font-bold mb-2" for="username">
-                  Username
+                <label className="mt-4 block text-white text-sm font-bold mb-2">
+                  Email
                 </label>
-                <input className="w-full py-1.5 px-4 mt-2 bg-slate-500/50 rounded-md outline-none font-semibold text-white focus:border-slate-800 focus:outline-cyan-400" id="username" type="text" placeholder="Username" />
+                <input className="w-full py-1.5 px-4 mt-2 bg-slate-500/50 rounded-md outline-none font-semibold text-white focus:border-slate-800 focus:outline-cyan-400" 
+                id="username" 
+                type="text" 
+                placeholder="Email" 
+                value={email} 
+                onChange = {handleEmailChange}
+                />
               </div>
-              <button className="py-1.5 px-4 mt-3 w-full  bg-cyan-400 text-black rounded-md font-medium ml-auto items-end hover:bg-cyan-600">
+              <button className="py-1.5 px-4 mt-3 w-full  bg-cyan-400 text-black rounded-md font-medium ml-auto items-end hover:bg-cyan-600" type='submit' >
                 Sign In
               </button>
 
@@ -32,10 +55,10 @@ const LoginPage = () => {
               <span class="px-3 font-medium    left-1/2 text-white">or</span>
               <span class="h-0.5  my-8 bg-gray-400 w-full border-0 "></span>
             </div>
-            <button className="py-1.5 px-4  flex items-center justify-center  w-full  bg-white shadow-sm text-black rounded-md font-medium hover:bg-cyan-600">
+            <button className="py-1.5 px-4  flex items-center justify-center  w-full  bg-white shadow-sm text-black rounded-md font-medium hover:bg-cyan-600" onClick={() => {login('google')}}>
               <FcGoogle class="text-2xl" />
-              <span className="text-sm ml-2">
-                SinIn with Google
+              <span className="text-sm ml-2" >
+                SignIn with Google
               </span>
             </button>
             <button className="py-1.5 px-4  flex items-center justify-center mt-3 w-full  bg-white shadow-sm text-black rounded-md font-medium hover:bg-cyan-600">
--- src/frontend/components/Navbar.js
+++ src/frontend/components/Navbar.js
@@ -1,12 +1,12 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { Link, NavLink } from "react-router-dom";
 import profile from './Public/profile.png';
 import { MdAccountCircle, MdKeyboardArrowRight } from 'react-icons/md';
-import { HiMenuAlt3,HiOutlineX } from 'react-icons/hi';
+import { HiMenuAlt3, HiOutlineX } from 'react-icons/hi';
 import { Transition } from '@headlessui/react';
 
 
-export const Navbar = () => {
+export const Navbar = ({account,onClickButton}) => {
 
     const [isOpen, setIsOpen] = useState(false);
     return (
@@ -25,89 +25,95 @@ export const Navbar = () => {
                     <li>
                         <Link className='mx-1 px-2 text-sm font-semibold'>About</Link>
                     </li>
-                   
-                    <li className="flex items-center justify-center ml-2 px-4 py-1 cursor-pointer text-sm text-blue-100 transition-colors duration-300 bg-blue-500 rounded-full shadow-xl hover:bg-blue-600 shadow-blue-400/30">
-                        <Link className='text-sm text-semibold px-2'>Connect Wallet</Link>
-                        <MdKeyboardArrowRight className='text-sm text-white-500' />
-                    </li>
-                </ul>
-                <div class="md:hidden flex items-center" onClick={()=>setIsOpen(!isOpen)}>
-                    <button class="outline-none p-2   mobile-menu-button bg-slate-500/30 rounded-full border-1 border-gray-500 select-none focus:bg-slate-800">
-                       {isOpen?<HiOutlineX className='text-2xl text-gray-200'/>:
-                       <HiMenuAlt3 className='text-2xl text-gray-200'/>
+                    <li className="flex items-center justify-center ml-2 px-4 py-1 cursor-pointer text-sm text-blue-100 transition-colors duration-300 bg-blue-500 rounded-full shadow-xl hover:bg-blue-600 shadow-blue-400/30" onClick={onClickButton}>
+                       {account === null ? (
+                        <span>Connect wallet</span>
+                       ):(
+                        <span>Connected</span>
+                       )
 
-                       }
+                       } 
+                        {/* <MdKeyboardArrowRight className='text-sm text-white-500' /> */}
+                    </li>
+
+                </ul>
+                <div class="md:hidden flex items-center" onClick={() => setIsOpen(!isOpen)}>
+                    <button class="outline-none p-2   mobile-menu-button bg-slate-500/30 rounded-full border-1 border-gray-500 select-none focus:bg-slate-800">
+                        {isOpen ? <HiOutlineX className='text-2xl text-gray-200' /> :
+                            <HiMenuAlt3 className='text-2xl text-gray-200' />
+
+                        }
                     </button>
                 </div>
-                
+
             </div>
-             {/* Menu  Mobile*/}
-             <Transition show={isOpen}
-                    enter="transition ease-out duration-100 transform"
-                    enterFrom="opacity-0 scale-95"
-                    enterTo="opacity-100 scale-100"
-                    leave="transition ease-in duration-75 transform"
-                    leaveFrom="opacity-100 scale-100"
-                    leaveTo="opacity-0 scale-95"
-                >
-                    {
-                        (ref) => (
-                            <div className="md:hidden " id="mobile-menu">
-                                <div
-                                    ref={ref}
-                                    className="dark:bg-transparent dark:text-white mx-4 pt-4 pb-4 space-y-1"
+            {/* Menu  Mobile*/}
+            <Transition show={isOpen}
+                enter="transition ease-out duration-100 transform"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="transition ease-in duration-75 transform"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+            >
+                {
+                    (ref) => (
+                        <div className="md:hidden " id="mobile-menu">
+                            <div
+                                ref={ref}
+                                className="dark:bg-transparent dark:text-white mx-4 pt-4 pb-4 space-y-1"
+                            >
+
+                                <Link
+                                    to="/listens"
+                                    activeClass="listens"
+                                    smooth={true}
+                                    offset={50}
+                                    duration={500}
+                                    className="cursor-pointer hover:bg-blue-900/30 text-black dark:text-gray-200 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                                 >
+                                    List ENS
+                                </Link>
+                                <Link
+                                    to="/buyens"
+                                    activeClass="buyens"
 
-                                    <Link
-                                        to="/listens"
-                                        activeClass="listens"
-                                        smooth={true}
-                                        offset={50}
-                                        duration={500}
-                                        className="cursor-pointer hover:bg-blue-900/30 text-black dark:text-gray-200 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                                    >
-                                        List ENS
-                                    </Link>
-                                    <Link
-                                        to="/buyens"
-                                        activeClass="buyens"
-                                        
-                                        smooth={true}
-                                        offset={50}
-                                        duration={500}
-                                        className="cursor-pointer hover:bg-blue-900/30 text-black dark:text-gray-200 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                                    >
-                                        Buy ENS
-                                    </Link>
+                                    smooth={true}
+                                    offset={50}
+                                    duration={500}
+                                    className="cursor-pointer hover:bg-blue-900/30 text-black dark:text-gray-200 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                                >
+                                    Buy ENS
+                                </Link>
 
-                                    <Link
-                                        to="/about"
-                                        activeClass="about"
-                                        smooth={true}
-                                        offset={50}
-                                        duration={500}
-                                        className="cursor-pointer hover:bg-blue-900/30 text-black dark:text-gray-200 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                                    >
-                                        About
-                                    </Link>
-                                    <Link
-                                        href="/contact"
-                                        activeClass="contact"
-                                        to="contact"
-                                        smooth={true}
-                                        offset={50}
-                                        duration={500}
-                                        className="cursor-pointer hover:bg-blue-900/30 text-black dark:text-gray-200 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                                    >
-                                        Connect Wallet
-                                    </Link>
-                                  
-                                </div>
+                                <Link
+                                    to="/about"
+                                    activeClass="about"
+                                    smooth={true}
+                                    offset={50}
+                                    duration={500}
+                                    className="cursor-pointer hover:bg-blue-900/30 text-black dark:text-gray-200 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                                >
+                                    About
+                                </Link>
+                                <Link
+                                    href="/contact"
+                                    activeClass="contact"
+                                    to="contact"
+                                    smooth={true}
+                                    offset={50}
+                                    duration={500}
+                                    className="cursor-pointer hover:bg-blue-900/30 text-black dark:text-gray-200 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                                >
+                                    Connect Wallet
+                                </Link>
+
                             </div>
-                        )
-                    }
+                        </div>
+                    )
+                }
 
-                </Transition>
-        </nav>
+            </Transition>
+        </nav >
     )
 }
